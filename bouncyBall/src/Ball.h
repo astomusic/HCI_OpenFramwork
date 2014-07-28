af04fac7e030ec94de7d62196e02c8f704095b2f
astomusic <astomusic@naver.com>
2014-07-29 00:35:43 +0900

    add bouncyball

---
+++ bouncyBall/src/Ball.h
@@ -0,0 +1,40 @@
+//
+//  Ball.h
+//  emptyExample
+//
+//  Created by astomusic on 2014. 7. 28..
+//
+//
+
+#pragma once
+
+#include "ofMain.h"
+
+class Ball
+{
+public:
+	Ball(void);
+	~Ball(void);
+    
+    void setGravity(float springin);
+	void setup(float xin, float yin, float din, int idin, float springin, float graivityin, float frictionin);
+    
+	void move();
+	void display();
+    
+public:
+	float x;
+	float y;
+	float diameter;
+	float vx;
+	float vy;
+	int id;
+    
+	float spring;
+	float gravity;
+	float friction;
+    
+	float width;
+	float height;
+    
+};
